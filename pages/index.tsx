0540227def3827d8df41c87fafe4fa9e802f1131
Marco Kang <kmnkit@gmail.com>
2022-01-20 20:41:02 +0900

    tailwindcss setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return null;
-}
+  return <div className="bg-pink-400">
+    <h1 className="text-green-400">It works!</h1>
+  </div>;
+};
 
 export default Home;
